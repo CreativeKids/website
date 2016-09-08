e09fa0c270c5c72f2125f6b67fb3b3cd0e4873aa
Rhys Moyne <rhys@creativekidssa.com.au>
2016-09-08 03:14:57 +0000

    rebuild pages at 2a324a7

--- projects/projects/travis-deploy.sh
+++ projects/projects/travis-deploy.sh
@@ -22,7 +22,7 @@ git fetch upstream
 git pull upstream master
 
 rm -rf projects/
-cp -R ../lektor_build/projects projects/
+cp -R ../lektor_build projects/
 
 git add -A .
 git commit -m "rebuild pages at ${rev}"
