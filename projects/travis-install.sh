2eb3b924b0f43bcf1720d2acf5b56382b6aab516
Rhys Moyne <rhys@creativekidssa.com.au>
2016-09-08 03:52:44 +0000

    rebuild pages at d972d44

--- projects/travis-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-pip install lektor && \
-git clone https://gitlab.com/rhysmoyne/school-website.git && \
-cd school-website && \
-git submodule init && \
-git submodule update && \
-git submodule update --remote content/projects && \
-git status
